5308541ce770bff4c5129c02e3b378446e82e49a
Sign0ret <adolfo.h.signoret@gmail.com>
2024-04-11 22:38:31 -0600

    5: all in

--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -4,6 +4,8 @@ import NextAuth, { DefaultSession } from "next-auth"
 export type ExtendedUser = DefaultSession["user"] & {
     id: string;
     role: UserRole;
+    isTwoFactorEnabled: boolean;
+    isOAuth: boolean;
 };
 
 declare module "next-auth" {
