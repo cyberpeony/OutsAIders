f45647e546b00a5ff6f2de4b8eb47c0d370f6c45
Sign0ret <adolfo.h.signoret@gmail.com>
2024-05-18 16:22:05 -0600

    auth

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,17 +1,10 @@
-import { Navbar } from "./_components/navbar";
-
 interface ProtectedLayoutProps {
     children: React.ReactNode;
 };
 
 const ProtectedLayout = ({children}: ProtectedLayoutProps) => {
     return (
-        <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-            <Navbar />
-            <div className="max-h-[400px]">
-                {children}
-            </div>
-        </div>
+        <div></div>
     )
 }
 
